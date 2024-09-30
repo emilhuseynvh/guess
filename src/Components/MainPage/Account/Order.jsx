d723e9d1e33404e2c769da742e1525fbef19048d
emilhuseynvh <emilhuseynvh@gmail.com>
2024-09-30 19:36:48 +0400

    account page updated

--- src/Components/MainPage/Account/Order.jsx
+++ src/Components/MainPage/Account/Order.jsx
@@ -3,7 +3,7 @@ import NoDataIcon from '../NoDataIcon'
 
 const Order = () => {
   return (
-    <div className='my-32 w-full mx-[120px]'>
+    <div className='my-32 lg:w-full mx-[120px]'>
       <NoDataIcon text='You havent placed any orders yet' />
     </div>
   )
